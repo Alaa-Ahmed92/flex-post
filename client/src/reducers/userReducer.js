71e66696bf16ac327f7e86b6ddf2a6c621989c8a
Alaa <alaa.ahmed.a92@gmail.com>
2022-05-21 01:14:07 +0100

    Add Search Feature

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -2,73 +2,105 @@ import { userConstants } from "../constants/userConsts";
 
 export const userReducer = (state = {}, action) => {
     switch (action.type) {
+        // Fetch Users
+        case userConstants.FETCH_USERS_REQUEST:
+            return {
+                ...state,
+                loading: true,
+            }
+        case userConstants.FETCH_USERS_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                users: action.users,
+                error: '',
+            }
+        case userConstants.FETCH_USERS_FAILURE:
+            return {
+                ...state,
+                loading: false,
+                error: action.error
+            }
         // Fetch User
         case userConstants.FETCH_USER_REQUEST:
             return {
+                ...state,
                 loading: true,
             }
         case userConstants.FETCH_USER_SUCCESS:
             return {
+                ...state,
                 loading: false,
                 data: action.user,
                 following: action.following
             }
         case userConstants.FETCH_USER_FAILURE:
             return {
+                ...state,
                 loading: false,
                 error: action.error
             }
         // Update User
         case userConstants.UPDATE_USER_REQUEST:
             return {
+                ...state,
                 loading: true,
             }
         case userConstants.UPDATE_USER_SUCCESS:
             return {
+                ...state,
                 loading: false,
                 updated: true,
                 data: action.user
             }
         case userConstants.UPDATE_USER_FAILURE:
             return {
+                ...state,
                 loading: false,
                 error: action.error
             }
         // Follow User
         case userConstants.FOLLOW_USER_REQUEST:
             return {
+                ...state,
                 loading: true,
             }
         case userConstants.FOLLOW_USER_SUCCESS:
             return {
+                ...state,
                 loading: false,
                 data: action.user,
                 following: action.following
             }
         case userConstants.FOLLOW_USER_FAILURE:
             return {
+                ...state,
                 loading: false,
                 error: action.error
             }
         // unFollow User
         case userConstants.UNFOLLOW_USER_REQUEST:
             return {
+                ...state,
                 loading: true,
             }
         case userConstants.UNFOLLOW_USER_SUCCESS:
             return {
+                ...state,
                 loading: false,
                 data: action.user,
                 following: action.following
             }
         case userConstants.UNFOLLOW_USER_FAILURE:
             return {
+                ...state,
                 loading: false,
                 error: action.error
             }
         // Delete User
         case userConstants.DELETE_USER:
             return {
+                ...state,
                 userDeleted: true,
             }
         // Find People
